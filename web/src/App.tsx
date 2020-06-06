4420cc5a13e4c8ec00c08b93b3d73b45d82899e0
Benicio <benicio.daniel@gmail.com>
2020-06-06 20:35:00 -0300

    Added Page Home Component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Oi! </h2>
-    </div>
+    <Home />
   );
 }
 
